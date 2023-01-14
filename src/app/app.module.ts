4e8c50fad2964f704bd9025eea7b6cf32fbded26
conceicao <conceicao-peres@hotmail.com>
2023-01-14 07:26:44 -0300

    pagina nao encontrada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FormsModule } from '@angular/forms';
 import { FooterComponent } from './footer/footer.component';
+import { NaoEncontradaComponent } from './nao-encontrada/nao-encontrada.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    NaoEncontradaComponent
   ],
   imports: [
     BrowserModule,
